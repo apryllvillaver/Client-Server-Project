de99a5d52a7e2eecdf988488d99a0b6f3ea249cf
apryllvillaver <deanapryll.villaver@g.msuiit.edu.ph>
2025-09-16 23:45:05 +0800

    Update server.py

--- server.py
+++ server.py
@@ -127,5 +127,7 @@ def register_event(event_id):
 # ------------------- MAIN -------------------
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    from os import environ
+    app.run(host="0.0.0.0", port=int(environ.get("PORT", 5000)))
+
 
